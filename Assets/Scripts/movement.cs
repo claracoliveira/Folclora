0f2aae7a397145c2b29ae87f3a9256c5180f6343
Ana Clara <61744513+claracoliveira@users.noreply.github.com>
2021-06-16 17:18:58 -0300

    Folclora 0.0.2
    pulo melhorado;

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-
     //Joystick
     protected Joystick joystick;
 
@@ -17,8 +16,13 @@ public class movement : MonoBehaviour
     // Pulo
     JoyButton joyButton;
     Rigidbody rb;
-    public bool jump;
-    public int jumpForce = 20;
+    // Checa se ele está pulando
+    private bool isJumping;
+    // Checa se o botão de pulo está pressionado
+    private bool isPressed;
+    // Checa se o personagem está no chão
+    private bool isGrounded;
+    public float jumpForce = 20f;
     public float rotationSpeed = 10;
 
     // Start is called before the first frame update
@@ -27,7 +31,6 @@ public class movement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         joystick = FindObjectOfType<Joystick>();
 
-
         // Instancia o botão de pulo
         joyButton = FindObjectOfType<JoyButton>();
     }
@@ -35,10 +38,6 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movimento
-        // Adiciona valores em X e Z baseado na velocidade de movimento e no joystick.
-        rb.velocity = new Vector3(joystick.Horizontal * moveSpeed, rb.velocity.y, joystick.Vertical * moveSpeed);
-
         // Rotação
         moveDirection = new Vector3(joystick.Horizontal * moveSpeed, 0, joystick.Vertical * moveSpeed);
         if(moveDirection != Vector3.zero)
@@ -47,27 +46,35 @@ public class movement : MonoBehaviour
             transform.rotation = Quaternion.Slerp (transform.rotation, qTo, Time.deltaTime * rotationSpeed);
         }
 
+        // Pulo
+        // Checa e troca o valor de "isPressed" para true ao apertar o botão de pulo
+        isPressed = joyButton.Pressed;
+    }
+
+    void FixedUpdate()
+    {
+        // Movimento
+        // Adiciona valores em X e Z baseado na velocidade de movimento e no joystick.
+        rb.velocity = new Vector3(joystick.Horizontal * moveSpeed, rb.velocity.y, joystick.Vertical * moveSpeed);
 
         // Pulo
-        // No primeiro if, limitamos a quantidade de pulo que o personagem pode chegar. Caso o "y" seja inferior a 40f, o personagem consegue
-        // pular, adicionando uma força ao Vector3 para cima e multiplicando pelo jumpForce (força de pulo).
-        if(!jump && joyButton.Pressed)
-        {
-            if(transform.position.y <= 40f)
-            {
-                jump = true;
-                rb.AddForce(Vector3.up * jumpForce);
-                
-            }
-
-            // O segundo if serve para o personagem não continuar pulando infinitamente. Caso o botão já esteja pressionado, ele seta jump como 
-            // "false".
-            if(jump && joyButton.Pressed)
-            {
-                jump = false;
-            }
+        // Se isPressed e isGrounded estiverem como "true", adiciona uma força em y
+        if(isPressed && isGrounded)
+        {  
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            // Seta isGrounded como false p/ que ele não pule mais
+            isGrounded = false;
         }
+    }
 
-        
+    // Função chamada toda vez que se "entra" em colisão
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.gameObject.CompareTag("Ground")) 
+        {
+            // Colidiu com o chão
+            isGrounded = true;
+        }
     }
 }
